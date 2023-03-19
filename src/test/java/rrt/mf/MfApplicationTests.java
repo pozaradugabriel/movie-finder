77be9bf807d331e252f37c8245f1321dc04b4b8e
pozaradugabriel <poza.radu@gmail.com>
2023-03-19 22:36:13 +0200

    initialise spring project

---
+++ src/test/java/rrt/mf/MfApplicationTests.java
@@ -0,0 +1,13 @@
+package rrt.mf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
